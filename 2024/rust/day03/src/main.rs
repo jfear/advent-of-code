adac20099171b836d63cbeb81588f8f27feca6ca
Justin Fear <justin.m.fear@gmail.com>
2024-12-03 17:00:29 -0500

    change day03 to use miette

--- 2024/rust/day03/src/main.rs
+++ 2024/rust/day03/src/main.rs
@@ -1,29 +1,39 @@
+use miette::miette;
 use regex::Regex;
 use std::io::{self, Write};
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-fn main() -> Result<()> {
+fn main() -> miette::Result<()> {
     let input = include_str!("../../../input_day03.txt");
 
-    writeln!(io::stdout(), "Part 1: {}", part1(input)?)?;
-    writeln!(io::stdout(), "Part 2: {}", part2(input)?)?;
+    writeln!(io::stdout(), "Part 1: {}", part1(input)?)
+        .map_err(|e| miette!("failed to write {}", e))?;
+
+    writeln!(io::stdout(), "Part 2: {}", part2(input)?)
+        .map_err(|e| miette!("failed to write {}", e))?;
+
     Ok(())
 }
 
-fn part1(input: &str) -> Result<String> {
+fn part1(input: &str) -> miette::Result<String> {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let mut total = 0;
     for line in input.lines() {
         for (_, [digit1, digit2]) in re.captures_iter(line).map(|c| c.extract()) {
-            let result = digit1.parse::<i32>()? * digit2.parse::<i32>()?;
-            total += result;
+            let digit1 = digit1
+                .parse::<i32>()
+                .map_err(|e| miette!("parse failed {}", e))?;
+
+            let digit2 = digit2
+                .parse::<i32>()
+                .map_err(|e| miette!("parse failed {}", e))?;
+
+            total += digit1 * digit2;
         }
     }
     Ok(total.to_string())
 }
 
-fn part2(input: &str) -> Result<String> {
+fn part2(input: &str) -> miette::Result<String> {
     let re1 = Regex::new(r"(don\'t|do|mul\(\d+,\d+\))").unwrap();
     let re2 = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let mut total = 0;
@@ -35,9 +45,16 @@ fn part2(input: &str) -> Result<String> {
                 "do" => enabled = true,
                 _ => {
                     if enabled {
-                        let m2 = re2.captures(command).unwrap();                        
-                        let digit1 = &m2[1].parse::<i32>()?;
-                        let digit2 = &m2[2].parse::<i32>()?;
+                        let m2 = re2.captures(command).unwrap();
+
+                        let digit1 = &m2[1]
+                            .parse::<i32>()
+                            .map_err(|e| miette!("parse failed {}", e))?;
+
+                        let digit2 = &m2[2]
+                            .parse::<i32>()
+                            .map_err(|e| miette!("parse failed {}", e))?;
+
                         total += digit1 * digit2;
                     }
                 }
@@ -52,14 +69,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1() -> Result<()> {
+    fn test_part1() -> miette::Result<()> {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!("161", part1(input)?);
         Ok(())
     }
 
     #[test]
-    fn test_part2() -> Result<()> {
+    fn test_part2() -> miette::Result<()> {
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         assert_eq!("48", part2(input)?);
         Ok(())
