39dfe8357c411cd3068f6755ca9d3dcee21ee4fd
Justin Fear <justin.m.fear@gmail.com>
2024-12-07 17:18:39 -0500

    fix benchmarks

--- 2024/rust/day07/benches/benchmarks.rs
+++ 2024/rust/day07/benches/benchmarks.rs
@@ -7,11 +7,16 @@ fn main() {
 
 #[divan::bench]
 fn part1() {
-    part1::process(divan::black_box(include_str!("../../../input_day07.txt"))).unwrap();
+    part1::process(&mut divan::black_box(include_str!(
+        "../../../input_day07.txt"
+    )))
+    .unwrap();
 }
 
 #[divan::bench]
 fn part2() {
-    todo!();
-    // part2::process(divan::black_box(include_str!("../../../input_day07.txt"))).unwrap();
+    part2::process(&mut divan::black_box(include_str!(
+        "../../../input_day07.txt"
+    )))
+    .unwrap();
 }
